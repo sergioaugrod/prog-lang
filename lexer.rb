e9d21f642bc44d76b98163d921c24e19f686dfc1
Sérgio Rodrigues <sergioaugrod@gmail.com>
2017-04-11 23:54:17 -0300

    Adds more tokens

--- lexer.rb
+++ lexer.rb
@@ -1,5 +1,14 @@
 class Lexer
   KEYWORDS = ['def', 'class', 'if', 'false', 'nil']
+  REGEX = {
+    identifier: /\A([a-z]\w*)/,
+    constant: /\A([A-Z]\w*)/,
+    number: /\A([0-9]+)/,
+    string: /\A([^"]*)"/,
+    operator: /\A(AND|OR|==|!=|<=|>=)/,
+    line: /\A\n+/,
+    whitespace: /\A /
+  }
 
   attr_reader :code, :tokens
 
@@ -22,9 +31,24 @@ class Lexer
   def parse_tokens(position)
     chunk = code[position..-1]
 
-    if identifier = chunk[/\A([a-z]\w*)/, 1]
+    if identifier = chunk[REGEX[:identifier], 1]
       position + token_identifier(identifier)
+    elsif constant = chunk[REGEX[:constant], 1]
+      position + token_constant(constant)
+    elsif number = chunk[REGEX[:number], 1]
+      position + token_number(number)
+    elsif string = chunk[REGEX[:string], 1]
+      position + token_string(string)
+    elsif operator = chunk[REGEX[:operator], 1]
+      position + token_operator(operator)
+    elsif chunk.match(REGEX[:line])
+      token_new_line
+      position += 1
+    elsif chunk.match(REGEX[:whitespace])
+      position += 1
     else
+      value = chunk[0, 1]
+      tokens << [value, value]
       position + 1
     end
   end
@@ -37,13 +61,35 @@ class Lexer
     end
     identifier.size
   end
+
+  def token_constant(constant)
+    tokens << [:CONSTANT, constant]
+    constant.size
+  end
+
+  def token_number(number)
+    tokens << [:NUMBER, number]
+    number.size
+  end
+
+  def token_string(string)
+    tokens << [:STRING, string]
+    string.size + 2
+  end
+
+  def token_operator(operator)
+    tokens << [operator, operator]
+    operator.size
+  end
+
+  def token_new_line
+    tokens << [:NEWLINE, "\n"]
+  end
 end
 
-code = """
-if a == 10 {
+code = """if a == 10 {
   a = 25
-}
-"""
+}"""
 
 lexer = Lexer.new(code)
 lexer.tokenize
